e3411c1733e5637f22cfffe70ff686d64d664c1d
Daniel Son <dson1204@gmail.com>
2020-05-21 03:29:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolate) {
   return chocolateBars.push(chocolate)
 }
+
+function destructivelyAddElementToBeginningOfArray(chocolate) {
+  return chocolateBars.unshift(chocolate)
+}
