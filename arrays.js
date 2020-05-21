5439fca9e481974592da6ac3ff4fbfeb36ac7a82
Daniel Son <dson1204@gmail.com>
2020-05-21 03:57:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyString = "foo"
 
 function addElementToBeginningOfArray(chocolateBars, candyString) {
-  return chocolateBars.push(chocolate)
+  return [candyString, ...chocolateBars]
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars) {
