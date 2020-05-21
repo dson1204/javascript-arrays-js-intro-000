71eee3631d004dafb890ec16bbb3fe1174df8d4a
Daniel Son <dson1204@gmail.com>
2020-05-21 03:48:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
   chocolateBars.push(candyString)
   return chocolateBars;
 }
+function accessElementInArray(array, index){
+  return array[index];
+}
