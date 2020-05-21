a80fb6dfe5d4ea823316d5556b493526ff92b152
Daniel Son <dson1204@gmail.com>
2020-05-21 03:55:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   chocolateBars.pop();
   return chocolateBars;
 }
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
